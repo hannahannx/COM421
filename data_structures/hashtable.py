e027ea1a372c43fcb849c4a0e3ffb16693ebb8d9
repl.it user <replituser@example.com>
2020-11-04 13:10:17 +0000

    Added code for data structures

--- data_structures/hashtable.py
+++ data_structures/hashtable.py
@@ -1,12 +1,37 @@
 #SEPERATE CHAINING MEETHOD 
-print("SEPERATE CHAINING METHOD ")
-print()
+#print("SEPERATE CHAINING METHOD ")
+#print()
 #Creating the list of keys 
-i = [36,88,54,28,49,21,63,7,19,2,11,41,34]
+#i = [36,88,54,28,49,21,63,7,19,2,11,41,34]
 #going through all of the values and calculating the has value
-for position in range(0,len(i),1):
-  if (i[position] == i[position]):
-    print ("KEY", i[position] ,"IS IN BUCKET ", (2*i[position]) % 17)
-    print ("", end = "")
-  else:
-    print ((2*i[position]) % 17)
+#for position in range(0,len(i),1):
+  #if (i[position] == i[position]):
+    #print ("KEY", i[position] ,"IS IN #BUCKET ", (2*i[position]) % 17)
+    #print ("", end = "")
+  #else:
+    #print ((2*i[position]) % 17)
+
+#Creating a class for the hashing function
+class HashTable:
+  #Initalising the class
+  def __init__(self,capacity = 127):
+    self.capacity = [None] * capacity
+  
+  def put(self,key,value):
+    hashCode = self.getHashCode(key)
+    calculation = hashCode % 127
+
+    
+  def get(self,key):
+    pass
+
+  def getHashCode(self,key):
+    for value in key:
+      convert = ord(value)
+      print("The value for {} is {}." .format(value,convert))
+
+  def run():
+    pass
+
+
+print(HashTable("a"))
