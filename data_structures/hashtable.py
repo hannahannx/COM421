b567df3d7e4bc0f5c806d6a0554b016e26696ef1
repl.it user <replituser@example.com>
2020-11-10 21:57:26 +0000

    Added code for a hash table

--- data_structures/hashtable.py
+++ data_structures/hashtable.py
@@ -19,32 +19,50 @@ class HashTable:
   
   #Making a method to put the values inside of the hashtable
   def put(self,key,value):
+    #calculating the hashCode for the value inputed
     hashCode = self.getHashCode(key)
     #Using this value and storing the value inside the list
     bucketIndex = hashCode % 127
+    #if there is nothing allocated inside the position then that position should become an empty list
     if self.bucketArray[bucketIndex] is None:
       self.bucketArray[bucketIndex] = []
-        #adding the values as a tuple onto the list
-      self.bucketArray.insert(bucketIndex,[key,value])
+        #adding the values (key and the value) as a tuple onto the list in that specific position
+      self.bucketArray[bucketIndex].append((key,value))
     print(self.bucketArray)
 
   def get(self,key):
-    pass
+    #Searchig for the item in the HashTable
+    hashCode = self.getHashCode(key)
+    bucketIndex = hashCode % 127
+    #Checking to see if there is something inside that bucket
+    if self.bucketArray[bucketIndex] is not None:
+      #loop through all of the postions in the value of the postions
+      for pairs in self.bucketArray[bucketIndex]:
+        if pairs[0] == key:# pairs is a tuple containing the value
+          return pairs[1]
+      #Shows that nothing has been found
+      return None 
 
   #Calculating the ASCII code for the key
   def getHashCode(self,key):
     hashCode = 0
+    #Looping through each character in the key and adding the ord value(ASCII) to the hashCode 
     for value in key:
       convert = ord(value)
       hashCode = hashCode + convert
     return hashCode
 
-
+  #Creating a function to run the HashTable
   def run():
-    pass
+    hashfunction = HashTable()
+    key = input("What value would you like to be used?")
+    description = input("Please describe {} ".format(key))
+    print("Displaying hashtable.........")
+    print()
+    hashfunction.put(key,description)
+    print(hashfunction.get(key))
+    
 
 #Main code
 #Testing for one value in the hashtable
-hashfunction = HashTable()
-
-hashfunction.put("cat","A type of animal")
+HashTable.run()
