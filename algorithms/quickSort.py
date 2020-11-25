4a20ba07b90cd80349720d9ae0a3aebeb85a44f0
repl.it user <replituser@example.com>
2020-11-25 17:06:37 +0000

    Added code for the quick sort

--- algorithms/quickSort.py
+++ algorithms/quickSort.py
@@ -1,1 +1,19 @@
-#importing module
+#importing modules
+
+#Creating the Hoare Partition function
+def hoarePartition(listToSort,start,end):
+  #Creating the pointers 
+  leftPoint = start
+  rightPoint = end
+  midpoint = math.floor((start + end)/2 )
+  pivot = listToSort[midpoint]
+  
+  #This will loop forever
+  while True:
+    while listToSort[leftPoint] < pivot:
+      leftPoint = leftPoint + 1 
+    while listToSort[rightPoint] > pivot
+      rightPoint = rightPoint - 1
+    
+if listToSort[leftPoint] != listToSort[rightPoint]:
+  
