d9a9b3d40ebc1d2a6a86adeb1ac897149c6aa9bd
repl.it user <replituser@example.com>
2020-12-15 20:26:05 +0000

    added index values to make it functional

--- algorithms/binarySearch.py
+++ algorithms/binarySearch.py
@@ -4,36 +4,28 @@ def binarySearch(listToCompare,numberToSearch):
   #Creating the indexes for the start and the end of the list
   start = 0
   end = len(listToCompare)-1
-  
+  num = -10
   #Checking to see how big the list is
   while start <= end :
     mid = int((start + end)/2)
     if listToCompare[mid] == numberToSearch:
       print("The value {} is at postion {}".format(numberToSearch,mid))
+      num = mid 
       break
-<<<<<<< HEAD
     else:
       if listToCompare[mid] > numberToSearch:
-        end = mid+1
-      elif listToCompare[mid] < numberToSearch:
-        start = mid-1
+        end = mid-1
       else:
-        print("Your number is not in the list")
-=======
-    elif listToCompare[mid] > numberToSearch:
-      end = mid+1
-    elif listToCompare[mid] < numberToSearch:
-      start = mid-1
-    else:
-      print("Your number is not in the list")
->>>>>>> c55cc374796d1d027b7fca4646dfe603672aeb5e
-
-
+        start = mid+1
+  return num
+  
 def run():
   #Asking for user input
   listToCompare = [i*i for i in range(1,101)]
   numberToSearch = int(input("Please enter a number to search for: "))
-  binarySearch(listToCompare,numberToSearch)
+  value = binarySearch(listToCompare,numberToSearch)
+  if value == -10:
+    print("Your number is not in the list")
 
 #Main Program
 run()
